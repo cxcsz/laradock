519c23ea6a71453db22cc3126c2b1d19bcf878fd
zhaoliangliang <2942847030@qq.com>
2024-09-05 11:26:53 +0800

    nginx 生成 crt 添加 cxcsz.test 用于本地测试

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -7,6 +7,13 @@ if [ ! -f /etc/nginx/ssl/default.crt ]; then
     chmod 644 /etc/nginx/ssl/default.key
 fi
 
+if [ ! -f /etc/nginx/ssl/cxcsz.crt ]; then
+    openssl genrsa -out "/etc/nginx/ssl/cxcsz.key" 2048
+    openssl req -new -key "/etc/nginx/ssl/cxcsz.key" -out "/etc/nginx/ssl/cxcsz.csr" -subj "/CN=cxcsz.test/O=cxcsz/C=CN"
+    openssl x509 -req -days 3650 -extfile <(printf "subjectAltName=DNS:cxcsz.test,DNS:*.cxcsz.test") -in "/etc/nginx/ssl/cxcsz.csr" -signkey "/etc/nginx/ssl/cxcsz.key" -out "/etc/nginx/ssl/cxcsz.crt"
+    chmod 644 /etc/nginx/ssl/cxcsz.key
+fi
+
 # Start crond in background
 crond -l 2 -b
 
